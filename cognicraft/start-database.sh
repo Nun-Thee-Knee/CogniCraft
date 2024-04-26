a49967c2771e5628837812555fe71f1354cf6868
Nandini Sharma <114645836+Nun-Thee-Knee@users.noreply.github.com>
2024-04-25 23:31:37 -0700

    Chore DB: Added the Prisma

--- cognicraft/start-database.sh
+++ cognicraft/start-database.sh
@@ -9,7 +9,7 @@
 
 # On Linux and macOS you can run this script directly - `./start-database.sh`
 
-DB_CONTAINER_NAME=".-postgres"
+DB_CONTAINER_NAME="cognicraft-postgres"
 
 if ! [ -x "$(command -v docker)" ]; then
   echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
@@ -48,6 +48,6 @@ fi
 docker run -d \
   --name $DB_CONTAINER_NAME \
   -e POSTGRES_PASSWORD="$DB_PASSWORD" \
-  -e POSTGRES_DB=. \
+  -e POSTGRES_DB=cognicraft \
   -p 5432:5432 \
   docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
